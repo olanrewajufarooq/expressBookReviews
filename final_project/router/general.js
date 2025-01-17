1445e7db1c2d5f53cced747c4b34456708901a72
Farooq Olanrewaju <olanrewajufarooq@yahoo.com>
2025-01-17 01:49:51 -0500

    [EDIT]: Using asynchronous calls for DB.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,11 +4,6 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-const doesExist = (username) => {
-    // Filter the users array for any user with the same username
-    
-}
-
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
@@ -30,24 +25,37 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Unable to register user."});
 });
 
+const fetchBooks = (isbn = null) => {
+    if (isbn) {
+        return books[isbn];  // Fetch the specific book by ISBN
+    } else {
+        return books;  // Return all books if no ISBN is provided
+    }
+};
+
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(200).json({"message": books});
+public_users.get('/', async function (req, res) {
+    try {
+        return res.status(200).json({"message": await fetchBooks()});
+    } catch {
+        return res.status(500).send("Failed to fetch books.")
+    }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
     const isbn = req.params.isbn;
-    return res.status(200).json({message: books[isbn]});
+
+    return res.status(200).json({message: await fetchBooks(isbn)});
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   const author = req.params.author;
-  for (let key in books) {
-    if (books[key].author === author) {
-        const booksByAuthor = books[key];
-        return res.status(200).json({message: booksByAuthor});
+  const gottenBooks = await fetchBooks();
+  for (let key in gottenBooks) {
+    if (gottenBooks[key].author === author) {
+        return res.status(200).json({message: gottenBooks[key]});
     }
   }
   return res.status(200).json({message: `No book by ${author} in database`});
@@ -55,16 +63,17 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   const title = req.params.title;
-  for (let key in books) {
-    if (books[key].title === title) {
-        const booksByTitle = books[key];
+  const gottenBooks = await fetchBooks();
+
+  for (let key in gottenBooks) {
+    if (gottenBooks[key].title === title) {
+        const booksByTitle = gottenBooks[key];
         return res.status(200).json({message: booksByTitle});
     }
   }
   return res.status(200).json({message: `No book by ${title} in database`});
-  
 });
 
 //  Get book review
